c8075617422fe6de185628336dc94dc2a1a0706d
dexter <49933542+Aman-Khan@users.noreply.github.com>
2023-01-27 13:37:16 +0530

    Create 6.2 - Introduction to DLL, learn about struct, and how is node represented.cpp

---
+++ 6.2 - Introduction to DLL, learn about struct, and how is node represented.cpp
@@ -0,0 +1,12 @@
+    Node* constructDLL(vector<int>& arr) {
+        Node* dummy = new Node(-1);
+        Node* tail = dummy;
+        
+        for(auto i:arr){
+            Node* temp = new Node(i);
+            tail->next =  temp;
+            temp->prev = tail;
+            tail = tail->next;
+        }
+        return dummy->next;
+    }
